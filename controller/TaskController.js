42a26f4adc46d8d957cb46367e93b8d28b459a6e
Willian Valentim <williann.valentim@gmail.com>
2022-03-07 16:00:39 -0300

    Conexão e Envio de tarefas para o BD

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,9 +1,32 @@
 // Controller - métodos
-const getAll = (req, res) => {
-    return res.render("index");
+const Task = require('../models/Task');
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasksList = await Task.find(); //pegar as listas lá do DB
+        return res.render("index", { tasksList }); // assim que voltar com os dados, ele executará o return
+    } catch (err) {
+        res.status(500).send({ error: err.message }); // caso não consiga acesso, informará o erro
+    }
+};
+
+const createTask = async (req, res) => {
+    const task = req.body;
+
+    if (!task.task) {
+        return res.redirect("/");
+    }
+
+    try {
+        await Task.create(task);
+        return res.redirect("/");
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
 };
 
 // Método da rota para exportação
 module.exports = {
-    getAll,
+    getAllTasks,
+    createTask,
 };
---
+++ database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectToDb = () => {
+    mongoose.connect(
+        "mongodb+srv://root:admin@todolist.8tbiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+        .then(() => console.log('MongoDB Atlas CONECTADO!'))
+        .catch((err) => console.log(err));
+};
+
+module.exports = connectToDb;
