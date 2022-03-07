5c4f81a18d3d150858c827bf9f87827ef12d9ad8
Willian Valentim <williann.valentim@gmail.com>
2022-03-07 17:37:26 -0300

    getbyid, atualização de tarefas e delete conf.

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -4,7 +4,7 @@ const Task = require('../models/Task');
 const getAllTasks = async (req, res) => {
     try {
         const tasksList = await Task.find(); //pegar as listas lá do DB
-        return res.render("index", { tasksList }); // assim que voltar com os dados, ele executará o return
+        return res.render("index", { tasksList, task: null, taskDelete: null }); // assim que voltar com os dados, ele executará o return
     } catch (err) {
         res.status(500).send({ error: err.message }); // caso não consiga acesso, informará o erro
     }
@@ -25,8 +25,45 @@ const createTask = async (req, res) => {
     }
 };
 
+const getById = async (req, res) => {
+    try {
+        const tasksList = await Task.find();
+        if (req.params.method == "update") {
+            const task = await Task.findOne({ _id: req.params.id });
+            res.render("index", { task, taskDelete: null, tasksList });
+        } else {
+            const taskDelete = await Task.findOne({ _id: req.params.id });
+            res.render("index", { task: null, taskDelete, tasksList });
+        }
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const updateOneTask = async (req, res) => {
+    try {
+        const task = req.body;
+        await Task.updateOne({ _id: req.params.id }, task);
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const deleteOneTask = async (req, res) => {
+    try {
+        await Task.deleteOne({ _id: req.params.id });
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+}
+
 // Método da rota para exportação
 module.exports = {
     getAllTasks,
     createTask,
+    getById,
+    updateOneTask,
+    deleteOneTask
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,8 @@ const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post('/create', TaskController.createTask); // post para habilitar a nova postagem
+routes.get('/getById/:id/:method', TaskController.getById);
+routes.post('/updateOneTask/:id', TaskController.updateOneTask);
+routes.get('/deleteOneTask/:id', TaskController.deleteOneTask);
 
 module.exports = routes;
